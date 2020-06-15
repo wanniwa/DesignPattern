ed07ba3c4d10195bbf33e42bdecd3a3b650b2488
wangning <wangning1@yunzhangfang.com>
2020-06-15 10:21:23 +0800

    策略模式-会员折扣计算1

--- src/main/java/com/wanniwa/dp/strategy/PriceCenter.java
+++ src/main/java/com/wanniwa/dp/strategy/PriceCenter.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class PriceCenter {
+
     /**
      * 对原价进行折扣计算
      * @param price 原价
@@ -12,18 +13,14 @@ public class PriceCenter {
      */
     public BigDecimal discount(BigDecimal price, String memberType){
         if ("普通会员".equals(memberType)) {
-            System.out.println("抱歉！普通会员没有折扣！");
-            return price;
+            System.out.println("普通会员没有折扣");
         }else if ("VIP会员".equals(memberType)) {
             System.out.println("vip会员打9折！");
             price = price.multiply(new BigDecimal("0.9")).setScale(2, RoundingMode.HALF_UP);
-            return price;
         }else if("超级VIP会员".equals(memberType)){
             System.out.println("超级VIP会员打5折！");
             price = price.multiply(new BigDecimal("0.5")).setScale(2,RoundingMode.HALF_UP);
-            return price;
         }
-        // 不支持的会员类型都是原价
         return price;
     }
 }
